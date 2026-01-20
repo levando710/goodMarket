6febe1726aacc2437b0841e6a642a6b93aa7a44d
levando710 <leedodo2k4@gmail.com>
2026-01-20 16:17:01 +0700

    Initial commit

--- src/main/java/edu/ptit/goodmarket/GoodMarketApplication.java
+++ src/main/java/edu/ptit/goodmarket/GoodMarketApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GoodMarketApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GoodMarketApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GoodMarketApplication.class, args);
+    }
 
 }
