0af0a419d797346985e8db04311a840dd554146c
levando710 <leedodo2k4@gmail.com>
2026-01-20 16:16:43 +0700

    Initial commit

---
+++ src/main/java/edu/ptit/goodmarket/GoodMarketApplication.java
@@ -0,0 +1,13 @@
+package edu.ptit.goodmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoodMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/ptit/goodmarket/GoodMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.ptit.goodmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoodMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
